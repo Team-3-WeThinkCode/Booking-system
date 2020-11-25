ea090bed1286ab17730cf06e920ea6c8f18caf04
bnkala <bnkala@student.wethinkcode.co.za>
2020-11-25 08:31:01 +0200

    changes to registration

--- registration.py
+++ registration.py
@@ -1,7 +1,8 @@
 import json
-user_info = {}
-j = json.dumps(user_info)
-def get_full_name(user_info, j):
+import getpass
+# user_info = {}
+# j = json.dumps(user_info)
+def get_full_name():
     '''
     This will ask the user to enter his or her first name and surname
     Return the first name and surname as the first name as the full name.
@@ -9,11 +10,11 @@ def get_full_name(user_info, j):
     full_name = input("Enter your first name and surname: ")
     while full_name == '':
         full_name = input("Enter your first name and surname: ")
-    user_info['name'] = full_name
+    # user_info['name'] = full_name
     
     return full_name
 
-def get_username(user_info, j):
+def get_username():
     '''
     This will prompt the user to enter the username
     '''
@@ -21,10 +22,10 @@ def get_username(user_info, j):
     while username == '':
         username = input("Enter your username: ")
     username = username.lower()
-    user_info['username'] = username
+    # user_info['username'] = username
     return username
 
-def get_email_address(username, user_info, j):
+def get_email_address(username):
     '''
     This will prompt the user to enter the email address, it will check if the name is exactly the same as the username
     and will also check if the domain is 'student.wethinkcode.co.za', otherwise the user must re-enter the email address
@@ -40,25 +41,23 @@ def get_email_address(username, user_info, j):
     while email_address.split('@')[1].lower() != 'student.wethinkcode.co.za':
         print('email domain not accurate')
         email_address = input("Enter your email address: ")
-    user_info['email address'] = email_address
+    # user_info['email address'] = email_address
     return email_address
 
-# def get_campus():
-#     campus = input("Enter campus (Cape Town or Johannesburg): ")
-#     return campus
 
-def get_password(user_info, j):
+def get_password():
     '''
     This will prompt the user to check the password entered is 8 characters in length or longer
     Otherwise the user must re-enter the password
     '''
+
     password = input("Enter your password (minimum 8 characters): ")
     while password == '':
         password = input("Enter your password (minimum 8 characters): ")
     while len(password) < 8:
         print("Password too short")
         password = input("Enter your password (minimum 8 characters):")
-    user_info['password'] = password
+    # user_info['password'] = password
     return password
 
 def validate_password(password):
@@ -72,12 +71,22 @@ def validate_password(password):
     
     return check_password
 
-if __name__  == "__main__":
-    name = get_full_name(user_info, j)
-    username = get_username(user_info, j)
-    email = get_email_address(username, user_info, j)
-    password = get_password(user_info, j)
+def adding_details():
+    name = get_full_name()
+    username = get_username()
+    email = get_email_address(username)
+    password = get_password()
     check_password = validate_password(password)
-    print(user_info)
-    print(j)
+    user_info = {"Full Name": name, 
+    "Username": username, 
+    "Email": email, 
+    "Password": password}
+
+    with open("student.json", "w") as outfile:
+        json.dump(user_info, outfile, indent=4)
+    
     print("Registration successful! Welcome to Code Clinic "+ username+".")
+
+
+if __name__  == "__main__":
+    adding_details()
