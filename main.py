228908b20fe69dc6940ed1fdc071e89e1b067a59
CarahPrinsloo <carahprinsloo@gmail.com>
2020-12-03 20:41:35 +0200

    Fixed and added validation checks

--- main.py
+++ main.py
@@ -1,47 +1,25 @@
 import sys
-from quickstart import create_service, check_calendar_connected
-import utilities as utils
+from validate_user_input import get_user_commands
 from commands import registration as register
+from quickstart import create_service
+import utilities as utils
 import do_commands
-
-
-def command_line_args():
-    #format: [username] [user_type] [command] [date] [start_time]
-    info = {'username' : sys.argv[1]}
-    if len(sys.argv) > 1:
-        for i in range(1, len(sys.argv)):
-            if sys.argv[i] == 'volunteer':
-                info['user_type'] = 'volunteer'
-            elif sys.argv[i] == 'patient':
-                info['user_type'] = 'patient'
-            elif sys.argv[i] == 'create':
-                info['command'] = 'create'
-            elif sys.argv[i] == 'cancel':
-                info['command'] = 'cancel'
-            elif sys.argv[i] == 'list-bookings':  #lists user code clinic bookings for next 7 days
-                info['command'] = 'list-bookings'
-            elif sys.argv[i] == 'list-slots': #lists all opens lots for next 7 days
-                info['command'] = 'list-slots'
-            if len(sys.argv[i]) == 26:
-                info['UD'] = sys.argv[i]
-    if len(sys.argv) == 6:
-        info['date'] = sys.argv[4]
-        info['start_time'] = sys.argv[5]
-    return info
   
         
 class Student:
     """ Setup student profile """
 
     def __init__(self):
-        self.info = command_line_args()
-        self.username = self.info['username']
-        try:
-            self.service = create_service(self.username)
-            print("Connected...")
-        except:
-            print("Student calendar could not connect.")
-            self.service = None
+        valid, self.info = get_user_commands()
+        self.username, self.service  = '', ''
+        if valid:
+            self.username = self.info['username']
+            try:
+                self.service = create_service(self.username)
+                print("Connected...")
+            except:
+                print("Student calendar could not connect.")
+                self.service = None
 
 
 class CodeClinic:
@@ -60,24 +38,25 @@ if __name__ == "__main__":
     student = Student()
     codeclinic = CodeClinic()
     execute = True
-    data = ''
+    output = 'INVALID: Input is invalid.\nUse the help command for the correct input format/commands.\nHelp command: [username] [-h]'
+    if not student.info:
+        execute = False
+        utils.print_output(output)
     if not sys.stdin.isatty():
         data = sys.stdin.readlines()
-    try:
-        utils.update_files(student.service, codeclinic.service, student.username)
-    except:
-        print("Something went wrong!")
-        execute = False
     if execute:
-        output = 'INVALID: Input is invalid.\nUse the help command for the correct input format/commands.\nHelp command: [username] [-h]'
-        if 'user_type' in student.info:
-            if student.info['user_type'] == 'volunteer':
-                output = do_commands.do_volunteer_commands(student, codeclinic, output)
-            elif student.info['user_type'] == 'patient':
-                output = do_commands.do_patient_commands(student, codeclinic, output)
-        elif 'command' in student.info and student.info['command'] == 'register':
-            if register.validate_registration_info(student.info):
-                added, output = register.add_info_to_json(student.info)
-        else:
-            output = do_commands.do_event_listing_commands(student, codeclinic, output)
-        utils.print_output(output)
+        try:
+            utils.update_files(student.service, codeclinic.service, student.username)
+            if 'user_type' in student.info:
+                if student.info['user_type'] == 'volunteer':
+                    output = do_commands.do_volunteer_commands(student, codeclinic, output)
+                elif student.info['user_type'] == 'patient':
+                    output = do_commands.do_patient_commands(student, codeclinic, output)
+            elif 'command' in student.info and student.info['command'] == 'register':
+                if register.validate_registration_info(student.info):
+                    added, output = register.add_info_to_json(student.info)
+            else:
+                output = do_commands.do_event_listing_commands(student, codeclinic, output)
+            utils.print_output(output)
+        except:
+            print("Something went wrong!")
--- validate_user_input.py
+++ validate_user_input.py
@@ -2,7 +2,32 @@ import sys
 import utilities as utils
 
 
+def get_username(info):
+    '''
+    Gets username from command line arguments and adds it to the given dictionary
+    :return: Dictionary with username if username in command line arguments
+    :return: Empty dictionary if username was not in command line arguments
+    '''
+
+    valid_args = ['create', 'cancel', 'volunteer', 'patient', 'list-bookings', 'list-open', 'list-slots']
+    lst_not_args = list(filter(lambda x: x not in valid_args, sys.argv))
+    if lst_not_args:
+        lst_username = list(filter(lambda y: y != 'main.py', lst_not_args))
+        for item in lst_username:
+            if not (('/' in item) or ('-' in item) or (':' in item)) and len(item)==9:
+                info['username'] = item
+                return info
+    return {}
+
+
 def get_user_type(info):
+    '''
+    Gets user type (volunteer/patient) from command line arguments and adds
+    it to the given dictionary
+    :return: Dictionary with user type if user type specified in command line arguments
+    :return: Given dictionary if user type was not specified in command line arguments
+    '''
+
     get_date, get_time, get_id = False, False, False
     if 'volunteer' in sys.argv:
         info['user_type'] = 'volunteer'
@@ -15,6 +40,13 @@ def get_user_type(info):
 
 
 def get_command(info, criteria):
+    '''
+    Gets command type (create/cancel/list-bookings/list-open/list-slots) from command line
+    arguments and adds it to the given dictionary
+    :return: Dictionary with command if command specified in command line arguments
+    :return: Given dictionary if command was not specified in command line arguments
+    '''
+
     if 'create' in sys.argv:
         info['command'] = 'create'
     if 'cancel' in sys.argv:
@@ -30,6 +62,13 @@ def get_command(info, criteria):
 
 
 def get_support(info, criteria):
+    '''
+    Gets support args (date/time/calendar id) from command line arguments and adds it
+    to the given dictionary
+    :return: Dictionary with support args if specified in command line arguments
+    :return: Given dictionary if support args was not specified in command line arguments
+    '''
+
     get_date, get_time, get_id = criteria[0], criteria[1], criteria[2]
     if get_date:
         if len(sys.argv) >= 5 and utils.check_date_format(sys.argv[4]):
@@ -37,24 +76,30 @@ def get_support(info, criteria):
         elif len(sys.argv) == 4 and utils.check_date_format(sys.argv[3]):
             info['date'] = sys.argv[3]
         else:
-            return False, info,'INVALID: Enter date in correct format.'
+            return False, info
     if get_time:
         if len(sys.argv) >= 6 and utils.check_time_format(sys.argv[5]):
             info['start_time'] = sys.argv[5]
         else:
-            return False, info, 'INVALID: Enter time in correct format.'
+            return False, info
     if 'date' in info and 'time' in info:
         if utils.date_has_passed(info['date'], info['time']):
-            return False, info, "INVALID: Date/time has already passed."
+            return False, info
     if get_id:
+        info['UD'] = ''
         if len(sys.argv[len(sys.argv)-1]) == 26:
             info['UD'] = sys.argv[len(sys.argv)-1]
-        else:
-            return False, info, "INVALID: Enter valid ID."
-    return True, info, ""
+            return True, info
+    return True, info
 
 
 def check_if_support_info_is_present(info):
+    '''
+    Confirms whether required information was given for the program to execute command
+    :return: True if required information was given
+    :return: False if required information was not given
+    '''
+
     if 'user_type' in info:
         if info['user_type'] == 'volunteer':
             if 'date' in info and 'start_time' in info:
@@ -75,16 +120,19 @@ def check_if_support_info_is_present(info):
 
 
 def get_user_commands():
-    info, criteria = get_user_type({})
-    info = get_command(info, criteria)
-    valid_format, info, output = get_support(info, criteria)
-    if valid_format and check_if_support_info_is_present(info):
-        return True, info
-    else:
-        print(output)
-        return False, {}
+    '''
+    Collects required information for the program to execute expected command
+    :return: True if valid information was collected; Dictionary with required information
+    :return: False if invalid information was collected; Empty Dictionary
+    '''
 
-
-if __name__ == "__main__":
-    valid, info = get_user_commands()
-    print(info)
+    info = get_username({})
+    if info:
+        info, criteria = get_user_type(info)
+        info = get_command(info, criteria)
+        valid_format, info = get_support(info, criteria)
+        if valid_format and check_if_support_info_is_present(info):
+            return True, info
+        else:
+            return False, {}
+    return False, {}
