fbc9fd5e6409549d4ec2f2b160acb15512b65950
bnkala <bnkala@student.wethinkcode.co.za>
2020-12-04 11:33:18 +0200

    login draft

--- commands/login.py
+++ commands/login.py
@@ -1,5 +1,25 @@
 import json
+from datetime import datetime
 
-f= open('data_files/.student.json')
-data = json.load(f)
-f.close()
+# user_info= open('data_files/.student.json', 'r')
+# user_data = json.load(user_info)
+
+# def write_json(data, filename= '.timestamp.json'):
+#     with open(filename, 'w') as f:
+#         json.dump(data, f, sort_keys= True, indent=4)
+
+# def user_login(user_info):
+#     with open('data_files/.student.json') as json_file:
+#         student_data = json.load(json_file)
+#         for student in student_data:
+#             if student['username'] == user_info['username'] and student['password'] == user_info['password']:
+
+def get_username():
+    return input("Enter your username: ")
+
+def get_password():
+    return input("Enter your password: ")
+
+# def login_details():
+#     with open('data_files/.student.json') as json_file:
+#         student_data = json.lo
