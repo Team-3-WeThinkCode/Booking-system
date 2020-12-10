466489b4f76b9a0509f80aca3a2bc8954e651254
jroy <jroy@student.wethinkcode.co.za>
2020-12-10 13:50:34 +0200

    improved gmail api

--- gmail_api.py
+++ gmail_api.py
@@ -13,9 +13,16 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
 
 def create_email_service():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
+    '''
+    Contacts google api services to create a service instance of the Gmail api.
+    This will be used to send emails from the code clinic Gmail account.
+
+            Parameters:
+                    NONE
+
+            Returns:
+                    Service (obj): service object of Gmail instance
+    '''
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -41,23 +48,47 @@ def create_email_service():
 
 
 def create_message(sender, to, subject, message_text):
-  message = MIMEText(message_text)
-  message['to'] = to
-  message['from'] = sender
-  message['subject'] = subject
-  raw_message = base64.urlsafe_b64encode(message.as_string().encode("utf-8"))
-  return {
-    'raw': raw_message.decode("utf-8")
-  }
+    '''
+    Encode and format the message_text given for safe sending to the Gmail API 
+    service, this is a requirement before sending emails.
+
+            Parameters:
+                    sender         (str): Email adress of sender
+                    to             (str): Email address of recipient
+                    subject        (str): Subject of email body
+                    message_text   (str): Message body of the email
+
+            Returns:
+                    Email_body     (dict): Encoded bit file of email body
+    '''
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    raw_message = base64.urlsafe_b64encode(message.as_string().encode("utf-8"))
+    return {
+      'raw': raw_message.decode("utf-8")
+    }
 
 
 def patient_create_text(username, event):
+    '''
+    Create a email body to be used with the Gmail API, message will be created 
+    and encoded in return. This message is for creating bookings
+
+            Parameters:
+                    username       (str): Username of patient
+                    event          (obj): Current event object
+
+            Returns:
+                    Email_body     (dict): Encoded bit file of email body
+    '''
     message = f"""
 Hello {split_username(event['attendees'][0]['email'])},
     
 A user has booked your open slot:
 
-Patient: {split_username(event['attendees'][1]['email'])}
+Patient: {username}
 Event date/time: {event['start']['dateTime'][:10]} \
 {event['start']['dateTime'][11:16]}
 Event ID: {event['id']}
@@ -76,32 +107,49 @@ WeThinkCode_ Code-clinic.
 
 
 def patient_cancel_text(username, event):
+    '''
+    Create a email body to be used with the Gmail API, message will be created 
+    and encoded in return. This message is for canceling bookings
+
+            Parameters:
+                    username       (str): Username of patient
+                    event          (obj): Current event object
+
+            Returns:
+                    Email_body     (dict): Encoded bit file of email body
+    '''
     message = f"""
 Hello {split_username(event['attendees'][0]['email'])},
     
 A user has canceled a booking with you:
 
+Patient: {username}
 Event date/time: {event['start']['dateTime'][:10]} \
 {event['start']['dateTime'][11:16]}
 Event ID: {event['id']}
 
 Please be aware that you are no longer expected to attend this code clinic.
 If another booking is made in place of this slot, you will be notified.
-    """
+"""
     return create_message("code.clinic.test@gmail.com",
                             event['attendees'][0]['email'],
                             "Code_Clinic booking canceled", message)
 
 
 def send_message(user_id, message, service):
-    """Send an email message.
+    '''
+    Contacts the Gmail API service to send an email with the given email body
 
-    Args:
-    service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
-    message: Message to be sent.
-"""
+            Parameters:
+                    service: (obj): Authorized Gmail API service instance.
+                    user_id: (str): User's email address. The special value "me"
+                                    can be used to indicate the authenticated user.
+                    message: (obj): Message to be sent.        
+
+            Returns:
+                    NONE
+    '''
+
     try:
         message = (service.users().messages().send(userId=user_id, body=message).execute())
     except:
